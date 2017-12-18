cdbfe14edc7fa8d14bae80db4dc6346281a79ced
Gábor Csárdi <csardi.gabor@gmail.com>
2017-12-18 16:43:38 +0000

    Test large files (#6)
    * Better large file support
    
    * Fix large file test on Windows
    
    * Make big file creation faster on macos

--- src/init.c
+++ src/init.c
@@ -7,11 +7,13 @@
 /* .Call calls */
 extern SEXP R_zip_list(SEXP);
 extern SEXP R_zip_zip(SEXP, SEXP, SEXP, SEXP);
+extern SEXP R_make_big_file(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"R_zip_list", (DL_FUNC) &R_zip_list, 1},
-  {"R_zip_zip",  (DL_FUNC) &R_zip_zip,  6},
-  {NULL, NULL, 0}
+  { "R_zip_list",      (DL_FUNC) &R_zip_list,      1 },
+  { "R_zip_zip",       (DL_FUNC) &R_zip_zip,       6 },
+  { "R_make_big_file", (DL_FUNC) &R_make_big_file, 2 },
+  { NULL, NULL, 0 }
 };
 
 void R_init_zip(DllInfo *dll) {
--- src/rzip.c
+++ src/rzip.c
@@ -45,13 +45,13 @@ SEXP R_zip_list(SEXP zipfile) {
 
   result = PROTECT(allocVector(VECSXP, 3));
   SET_VECTOR_ELT(result, 0, allocVector(STRSXP, num_files));
-  SET_VECTOR_ELT(result, 1, allocVector(INTSXP, num_files));
-  SET_VECTOR_ELT(result, 2, allocVector(INTSXP, num_files));
+  SET_VECTOR_ELT(result, 1, allocVector(REALSXP, num_files));
+  SET_VECTOR_ELT(result, 2, allocVector(REALSXP, num_files));
 
   for (i = 0; i < num_files; ++i) {
     SET_STRING_ELT(VECTOR_ELT(result, 0), i, mkChar(files[i]));
-    INTEGER(VECTOR_ELT(result, 1))[i] = compressed_size[i];
-    INTEGER(VECTOR_ELT(result, 2))[i] = uncompressed_size[i];
+    REAL(VECTOR_ELT(result, 1))[i] = compressed_size[i];
+    REAL(VECTOR_ELT(result, 2))[i] = uncompressed_size[i];
     free(files[i]);
   }
   free(files);
@@ -60,4 +60,119 @@ SEXP R_zip_list(SEXP zipfile) {
 
   UNPROTECT(1);
   return result;
+}
+
+#ifdef _WIN32
+#include <windows.h>
+
+int zip__utf8_to_utf16_alloc(const char* s, WCHAR** ws_ptr) {
+  int ws_len, r;
+  WCHAR* ws;
+
+  ws_len = MultiByteToWideChar(
+    /* CodePage =       */ CP_UTF8,
+    /* dwFlags =        */ 0,
+    /* lpMultiByteStr = */ s,
+    /* cbMultiByte =    */ -1,
+    /* lpWideCharStr =  */ NULL,
+    /* cchWideChar =    */ 0);
+
+  if (ws_len <= 0) { return GetLastError(); }
+
+  ws = (WCHAR*) R_alloc(ws_len,  sizeof(WCHAR));
+  if (ws == NULL) { return ERROR_OUTOFMEMORY; }
+
+  r = MultiByteToWideChar(
+    /* CodePage =       */ CP_UTF8,
+    /* dwFlags =        */ 0,
+    /* lpMultiByteStr = */ s,
+    /* cbMultiBytes =   */ -1,
+    /* lpWideCharStr =  */ ws,
+    /* cchWideChar =    */ ws_len);
+
+  if (r != ws_len) {
+    error("processx error interpreting UTF8 command or arguments");
+  }
+
+  *ws_ptr = ws;
+  return 0;
+}
+
+#endif
+
+#ifdef __APPLE__
+#include <fcntl.h>
+#include <unistd.h>
+#endif
+
+SEXP R_make_big_file(SEXP filename, SEXP mb) {
+
+#ifdef _WIN32
+
+  const char *cfilename = CHAR(STRING_ELT(filename, 0));
+  WCHAR *wfilename = NULL;
+  LARGE_INTEGER li;
+
+  if (zip__utf8_to_utf16_alloc(cfilename, &wfilename)) {
+    error("utf8 -> utf16 conversion");
+  }
+
+  HANDLE h = CreateFileW(
+    wfilename,
+    GENERIC_WRITE,
+    FILE_SHARE_DELETE,
+    NULL,
+    CREATE_NEW,
+    FILE_ATTRIBUTE_NORMAL,
+    NULL);
+  if (h == INVALID_HANDLE_VALUE) error("Cannot create big file");
+
+  li.QuadPart = INTEGER(mb)[0] * 1024.0 * 1024.0;
+  li.LowPart = SetFilePointer(h, li.LowPart, &li.HighPart, FILE_BEGIN);
+
+  if (0xffffffff == li.LowPart && GetLastError() != NO_ERROR) {
+    CloseHandle(h);
+    error("Cannot create big file");
+  }
+
+  if (!SetEndOfFile(h)) {
+    CloseHandle(h);
+    error("Cannot create big file");
+  }
+
+  CloseHandle(h);
+
+#endif
+
+#ifdef __APPLE__
+
+  const char *cfilename = CHAR(STRING_ELT(filename, 0));
+  int fd = open(cfilename, O_WRONLY | O_CREAT);
+  double sz = INTEGER(mb)[0] * 1024.0 * 1024.0;
+  fstore_t store = { F_ALLOCATECONTIG, F_PEOFPOSMODE, 0, sz };
+  // Try to get a continous chunk of disk space
+  int ret = fcntl(fd, F_PREALLOCATE, &store);
+  if (-1 == ret) {
+    // OK, perhaps we are too fragmented, allocate non-continuous
+    store.fst_flags = F_ALLOCATEALL;
+    ret = fcntl(fd, F_PREALLOCATE, &store);
+    if (-1 == ret) error("Cannot create big file");
+  }
+
+  if (ftruncate(fd, sz)) {
+    close(fd);
+    error("Cannot create big file");
+  }
+
+  close(fd);
+
+#endif
+
+#ifndef _WIN32
+#ifndef __APPLE__
+  error("cannot create big file (only implemented for windows and macos");
+#endif
+#endif
+
+  return R_NilValue;
 }
