fc9b82715f5f849aaf5ea71edc1fc3b1e8c15abb
Gábor Csárdi <csardi.gabor@gmail.com>
2019-02-22 18:43:04 +0000

    Unzip process

--- R/process.R
+++ R/process.R
@@ -19,3 +19,35 @@ get_tool <- function (prog) {
 unzip_exe <- function() {
   get_tool("unzip")
 }
+
+zip_data <- new.env(parent = emptyenv())
+
+#' Create an external unzip process
+#'
+#' @param zipfile Path to the zip file to uncompress.
+#' @param exdir Directory to uncompress the archive to. If it does not
+#'   exist, it will be created.
+#' @return An unzip process object, its class inherits from the
+#'   [processx::process] class.
+#'
+#' @export
+
+make_unzip_process <- function(zipfile, exdir) {
+  need_packages(c("processx", "R6"), "creating unzip processes")
+  unzip_class <- zip_data$unzip_class %||%
+    R6::R6Class(
+      "unzip_process",
+      inherit = processx::process,
+      public = list(
+        initialize = function(zipfile, exdir) {
+          super$initialize(unzip_exe(), c(zipfile, exdir))
+        }
+      ),
+      private = list(
+        zipfile = NULL,
+        exdir = NULL
+      )
+    )
+
+  unzip_class$new(zipfile, exdir)
+}
---
+++ tests/testthat/test-unzip-process.R
@@ -0,0 +1,20 @@
+
+context("unzip_process")
+
+test_that("unzip_process", {
+  z <- make_a_zip()
+  tmp2 <- test_temp_dir()
+  p1 <- make_unzip_process(z$zip, tmp2)
+  p1$wait(2000)
+  p1$kill()
+
+  expect_equal(p1$get_exit_status(), 0)
+
+  expect_true(file.exists(file.path(tmp2, basename(z$ex), "file1")))
+  expect_true(file.exists(file.path(tmp2, basename(z$ex), "dir")))
+  expect_true(file.exists(file.path(tmp2, basename(z$ex), "dir", "file2")))
+
+  expect_equal(readLines(file.path(tmp2, basename(z$ex), "file1")), "file1")
+  expect_equal(
+    readLines(file.path(tmp2, basename(z$ex), "dir", "file2")), "file2")
+})
