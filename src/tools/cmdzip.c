2a746d839008a864a422f886d3fc0a842c7441a8
Gábor Csárdi <csardi.gabor@gmail.com>
2019-07-22 15:17:20 +0100

    Exit with non-zero in processes, on error

--- src/tools/cmdzip.c
+++ src/tools/cmdzip.c
@@ -9,6 +9,12 @@
 void cmd_zip_error_handler(const char *reason, const char *file,
 			   int line, int zip_errno, int eno) {
   fprintf(stderr, "zip error: `%s` in file `%s:%i`\n", reason, file, line);
+  if (eno < 0) {
+    eno = - eno;
+  } else if (eno == 0) {
+    eno = 1;
+  }
+  exit(eno);
 }
 
 int main(int argc, char* argv[]) {
